dfe7de87830b98696b5042718cc77dbe91ca09d3
ÂYaroslav Shishkin <company-rosstan@yandex.ru>
2022-01-27 21:17:40 +0300

    Первое задание

---
+++ script.js
@@ -0,0 +1,9 @@
+let title = "Привет! Это мой первый урок по JS";
+let screens = "Задание выполнено";
+let screenPrice = 4;
+let rollback = 4;
+let fullPrice = 544;
+let adaptive = 4;
+
+alert(title);
+console.log(screens);
