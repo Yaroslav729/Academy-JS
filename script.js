a4b18cceb0057c9901a14f6d272506a3548873e0
ÂYaroslav Shishkin <company-rosstan@yandex.ru>
2022-01-27 21:40:12 +0300

    commit в новой ветке

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 let title = "Привет! Это мой первый урок по JS";
 let screens = "Задание выполнено";
-let screenPrice = 4;
+let screenPrice = 576+322;
 let rollback = 4;
 let fullPrice = 544;
 let adaptive = 4;
 
 alert(title);
 console.log(screens);
+console.log(screenPrice );
